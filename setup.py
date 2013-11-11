4b66ff5027a4894acca84ae4aea953cd7064e95e
cgtx <carl@carlgeorge.us>
2013-11-11 12:51:29 -0600

    change setup for single file module

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='supernova-keyring-helper',
     version='0.4',
     description=('Store all required information for a Rackspace '
@@ -8,13 +8,19 @@ setup(
     author='Carl George',
     author_email='carl@carlgeorge.us',
     url='https://github.com/cgtx/supernova-keyring-helper',
-    packages=['supernova_keyring_helper'],
+    license='Apache License, Version 2.0',
+    py_modules=['supernova_keyring_helper'],
     install_requires=['supernova', 'rackspace-novaclient'],
     entry_points={
         'console_scripts': [
-            'supernova-keyring-helper = supernova_keyring_helper.shell:main'
+            'supernova-keyring-helper=supernova_keyring_helper:main'
         ]
-    }
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux'
+    ]
 )
 
 # vim: set syntax=python sw=4 ts=4 expandtab :
--- supernova_keyring_helper/__init__.py
+++
@@ -1,1 +0,0 @@
-# vim: set syntax=python sw=4 ts=4 expandtab :
